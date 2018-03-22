e38ca3f8f765d6232e04d8a78350cdbedff136c6
jsta <stachel2@msu.edu>
2018-03-22 11:43:09 -0400

    unzip to versioned path

--- R/utils.R
+++ R/utils.R
@@ -2,9 +2,10 @@ cache_path <- function() paste0(rappdirs::user_data_dir(
   appname   = "gshhg",
   appauthor = "gshhg"), .Platform$file.sep)
 
-get_if_not_exists <- function(url, destfile){
+get_if_not_exists <- function(url, destfile, versioned_path){
   if(!file.exists(destfile)){
     download.file(url, destfile)
+    unzip(destfile, exdir = versioned_path)
   }else{
     message(paste0("A local copy of ", url, " already exists on disk"))
   }
